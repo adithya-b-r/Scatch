d8fb210df0cad8fa1d5235e509bd58efff00087b
Abr009 <bradithya945@gmail.com>
2024-10-23 10:31:12 +0530

    Added app.js

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, (err) => {
+  if (err)
+    console.log(err);
+  else
+    console.log("Server is running: http://localhost:3000");
+})
